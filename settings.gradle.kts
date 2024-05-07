9cefc7da7b3cb568dcbc6a70ee76efe5433ddc94
nohayla <anoada.nohayla2001@gmail.com>
2024-05-07 16:14:48 +0100

    firstt commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Anoada_Nohayla_Tp8"
+include(":app")
+ 
